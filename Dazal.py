f65246ecae7da73f301993c86224d2622c91a99b
dy1103 <48409171+dy1103@users.noreply.github.com>
2019-03-10 22:53:06 +0900

    Update Dazal.py

--- Dazal.py
+++ Dazal.py
@@ -10,6 +10,12 @@ async def on_ready():
     print("login")
     print("-------------------")
     await client.change_presence(game=discord.Game(name='!명령어', type=1))
+    
+    
+@client.event
+async def on_message(message):
+    if message.content.startswith('!명령어'):
+        await client.send_message(message.channel,"명령어, 트위치, 시참, 카페, 유튜브")
 
 
 @client.event
@@ -34,12 +40,7 @@ async def on_message(message):
 async def on_message(message):
     if message.content.startswith('!시참'):
         await client.send_message(message.channel,"주소 : handazal.kro.kr / 1.7.10 + 디코 시참방 무조건 들어와야 함")
-
-
-@client.event
-async def on_message(message):
-    if message.content.startswith('!명령어'):
-        await client.send_message(message.channel,"명령어, 시참, 카페, 유튜브, 트위치")
+        
 
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
