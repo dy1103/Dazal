cc44ee1cd6c4b52484bc4a98e6775c87db5d4fd5
dy1103 <48409171+dy1103@users.noreply.github.com>
2019-03-10 22:33:49 +0900

    Update Dazal.py

--- Dazal.py
+++ Dazal.py
@@ -22,6 +22,24 @@ async def on_message(message):
 async def on_message(message):
     if message.content.startswith('!유튜브'):
         await client.send_message(message.channel,"https://www.youtube.com/channel/UCntJ_88iKtM0LE4dX5ovqFw?view_as=subscriber")
+        
+        
+@client.event
+async def on_message(message):
+    if message.content.startswith('!카페'):
+        await client.send_message(message.channel,"https://cafe.naver.com/twitchdazal")
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!시참'):
+        await client.send_message(message.channel,"주소 : handazal.kro.kr / 1.7.10 + 디코 시참방 무조건 들어와야 함")
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!명령어'):
+        await client.send_message(message.channel,"!명령어, !시참, !카페, !유튜브, !트위치")
 
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
