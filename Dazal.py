961b200a665dd31c27156e446bee9a4a4e884a0e
dy1103 <48409171+dy1103@users.noreply.github.com>
2019-03-11 19:57:10 +0900

    Update Dazal.py

--- Dazal.py
+++ Dazal.py
@@ -14,16 +14,16 @@ async def on_ready():
     
 @client.event
 async def on_message(message):
-    if message.content('!트위치'):
+    if message.content.startswitch('!트위치'):
         await client.send_message(message.channel, "https://www.twitch.tv/dazal3635")
 
-    if message.content('!유튜브'):
+    if message.content.startswitch('!유튜브'):
         await client.send_message(message.channel, "https://www.youtube.com/channel/UCntJ_88iKtM0LE4dX5ovqFw?view_as=subscriber")
 
-    if message.content('!카페'):
+    if message.content.startswitch('!카페'):
         await client.send_message(message.channel, "https://cafe.naver.com/twitchdazal")
 
-    if message.content('!시참'):
+    if message.content.startswitch('!시참'):
         await client.send_message(message.channel, "주소 : handazal.kro.kr / 1.7.10 + 디코 시참방 무조건 들어와야 함")
 
     if message.content == '!명령어':
