2114413357e3c74b52f51f51daf5cee8e770a530
dy1103 <48409171+dy1103@users.noreply.github.com>
2019-03-11 19:53:21 +0900

    Update Dazal.py

--- Dazal.py
+++ Dazal.py
@@ -14,29 +14,20 @@ async def on_ready():
     
 @client.event
 async def on_message(message):
-    if message.content.startswith('!명령어'):
-        await client.send_message(message.channel, "명령어, 트위치, 시참, 카페, 유튜브")
-        
-    if message.content.startswith('!트위치'):
+    if message.content('!트위치'):
         await client.send_message(message.channel, "https://www.twitch.tv/dazal3635")
-        
-    if message.content.startswith('!유튜브'):
+
+    if message.content('!유튜브'):
         await client.send_message(message.channel, "https://www.youtube.com/channel/UCntJ_88iKtM0LE4dX5ovqFw?view_as=subscriber")
-        
-    if message.content.startswith('!카페'):
+
+    if message.content('!카페'):
         await client.send_message(message.channel, "https://cafe.naver.com/twitchdazal")
-        
-    if message.content.startswith('!시참'):
+
+    if message.content('!시참'):
         await client.send_message(message.channel, "주소 : handazal.kro.kr / 1.7.10 + 디코 시참방 무조건 들어와야 함")
-        
-    if message.content.startswith('!선택'):
-        choice = message.content.split(" ")
-        choicenumber = random.randint(1, len(choice)-1)
-        choiceresult = choice[choicenumber]
-        await client.send_message(message.channel, choiceresult)
-        
-    if message.content == '!커맨드':
-        await client.send_message(message.channel,"답변")
+
+    if message.content == '!명령어':
+        await client.send_message(message.channel,"")
         embed = discord.Embed(title="명령어", description="!트위치, !시참, !카페, !유튜브", color=0x00ff00)
         embed.set_footer(text = "DaZal★Bot")
         await client.send_message(message.channel, embed=embed)
