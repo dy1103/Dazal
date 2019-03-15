556654e1c67ace4bc1455dac417766bd486d0012
dy1103 <48409171+dy1103@users.noreply.github.com>
2019-03-15 18:28:40 +0900

    Update Dazal.py

--- Dazal.py
+++ Dazal.py
@@ -26,10 +26,7 @@ async def on_message(message):
         await client.send_message(message.channel, "주소 : handazal.kro.kr / 1.7.10 + 디코 시참방 무조건 들어와야 함")
 
     if message.content.startswitch('!명령어'):
-        await client.send_message(message.channel,"명령어 ")
-        embed = discord.Embed(title="명령어", description="!트위치, !시참, !카페, !유튜브", color=0x00ff00)
-        embed.set_footer(text = "DaZal★Bot")
-        await client.send_message(message.channel, embed=embed)
+        await client.send_message(message.channel,"명령어, 카페, 시참, 유튜브, 트위치")
 
     
        
